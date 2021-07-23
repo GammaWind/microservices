2ee6efd283bf426b761fbfe2632821b623e4d48d
niharpawade@outlook.com <niharpawade@outlook.com>
2021-07-23 09:22:49 +0530

    added spring cloud server

---
+++ microservices/config-server/src/main/java/com/microservices/config/server/ConfigServer.java
@@ -0,0 +1,16 @@
+package com.microservices.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableConfigServer
+@SpringBootApplication
+@ComponentScan(basePackages = "com.microservices")
+public class ConfigServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
